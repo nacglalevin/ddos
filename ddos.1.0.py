31c768a40b1959e4ed6b93a3313039b5d3484d71
Lalevin <167055481+nacglalevin@users.noreply.github.com>
2024-04-20 17:33:54 +0800

    更新 ddos.1.0.py

--- ddos.1.0.py
+++ ddos.1.0.py
@@ -2,7 +2,7 @@
 ========================================
 Name:DDos   Author: Lalevin Martin
  Mailbox: zzlyxht@outlook.com                                                
- Github: http://github.com/su-57g  
+ Github: http://github.com/nacglalevin 
 Written in 2023-5-3
 ==================NACG==================
 """
